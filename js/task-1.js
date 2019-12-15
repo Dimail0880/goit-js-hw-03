feec83d585c0c7188605c180675824228f045e9f
dimail0880 <dimail4888@gmail.com>
2019-12-15 22:59:58 +0200

    module-3 done

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "Mango",
+    age: 20,
+    hobby: "html",
+    premium: true
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+for (let [key, value] of Object.entries(user)) {
+    console.log(`${key} : ${value}`);
+}
